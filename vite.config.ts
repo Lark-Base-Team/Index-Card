c78abe916efdc71dafca9015a5e42fa6be89f303
谢晓智 <xiexiaozhi@bytedance.com>
2024-06-06 18:00:01 +0800

    feat: 打包结果

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,17 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
     base: "./",
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: (id) => {
+                    if (id.includes('node_modules')) {
+                        return 'vendor'
+                    }
+                }
+            }
+        }
+    },
     plugins: [
         react(),
         svgr(),
